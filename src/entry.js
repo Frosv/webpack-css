7100f9a010dfd5965424b0b4c9b3bac8c08368b6
frosv <616342684@qq.com>
2017-04-16 21:42:10 +0800

    修改文件路径

--- src/entry.js
+++ src/entry.js
@@ -1,10 +1,10 @@
 //引入css
-require('./nav.css');//导航栏
-require('./index.css');//主体
+require('./css/nav.css');//导航栏
+require('./css/index.css');//主体
 // require('./left.css');//主体
 // require('./right.css');//主体
-require('./footer.css');//底部
+require('./css/footer.css');//底部
 
 
 //引入js
-require('./index.js');
+require('./js/index.js');
